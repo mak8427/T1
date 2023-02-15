dba59c43fd06e3c62face748c5af3cfa4a1554ef
Davide mattioli <davide.mattioli.2001@gmail.com>
2023-02-15 12:05:36 +0100

    Initialised the dens layer

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
-# This is a sample Python script.
+import  numpy as np
+import matplotlib.pyplot as plt
+def main():
+    #build a neural network from scratch
+    #define the input data as a sin function of shape (1,100) with 70% missing values from 0 to 2
+    x = np.sin(np.linspace(0,2*np.pi,100)).reshape(1,100)
+    x = np.where(np.random.rand(1,100) < 0.1, x, np.nan)
+    #plot the input data
+    plt.scatter(list(range(0,len(x[0]))),x[0])
+    plt.show()
+    #activation function
+    def sigmoid(x):
+        return 1 / (1 + np.exp(-x))
+    #the architecture of the neural network will be the following:
+    #input layer: 1 neuron
+    #3 hidden layers: 10 neurons each
+    #output layer: 1 neuron
+    #the weights and biases will be initialized randomly
+    #the weights and biases will be updated using gradient descent
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+    class Layer_Dense:
+        def __init__(self, n_inputs, n_neurons): #initialize the weights and biases
+            self.weights = 0.1 * np.random.randn(n_inputs, n_neurons) #initialize the weights to random values
+            self.biases = np.zeros((1, n_neurons)) #initialize the biases to 0
+        def forward(self, inputs):#forward propagation (calculate the output of the layer)
+            self.output = np.dot(inputs, self.weights) + self.biases #calculate the output of the layer
+        def backward(self, dvalues):#backward propagation (calculate the gradients)
+            self.dweights = np.dot(self.inputs.T, dvalues) #
+            self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+            self.dinputs = np.dot(dvalues, self.weights.T)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
+
+
+
+
+
+
+
+
+
+    return
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
+#
